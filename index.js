f5992c33203060363174b8b096ea90994fb7cac6
jin li <jinyuli91@gmail.com>
2018-08-25 13:28:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const driver = 'bob';
+let driver = 'bob';
+
+console.log("this code was called");
+console.log(driver);
+console.log(driver, 'is the driver variable name');
+// don't know what they below console.log doesn't work
+// console.log(`${driver}, is the driver variable name`);
